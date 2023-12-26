397a6b9e2dd853b5a4652c8b7b0d2513d117c583
AvashDahal <102864547+AvashDahal@users.noreply.github.com>
2023-12-26 05:58:36 +0545

    fix conflict

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const connectFlash = require('connect-flash')
  const session = require('express-session')
 
 
+
 //initialization
 const app = express()
 app.use(morgan('dev'))
@@ -23,27 +24,27 @@ app.use(express.urlencoded({extended:false}))
 //const passport = require('passport')
 
 //init session 
-app.use(session({
-    secret:process.env.SECRET_SESSION,
-    resave: false,
-    saveUninitialized:false,
-    cookie:{
-        secure:true,
-        httpOnly:true
+// app.use(session({
+//     secret:process.env.SECRET_SESSION,
+//     resave: false,
+//     saveUninitialized:false,
+//     cookie:{
+//         //secure:true
+//         httpOnly:true
 
-    }
-}))
+//     }
+// }))
 
 // const storage = multer.memoryStorage();
 // const upload = multer({ storage: storage });
 
 
 
-app.use(connectFlash());
- app.use((req, res, next) => {
-  res.locals.messages = req.flash();
-  next();
-});
+// app.use(connectFlash());
+// app.use((req, res, next) => {
+//   res.locals.messages = req.flash();
+//   next();
+// });
 
 //routes
 app.use('/',require('./routes/index.routes'))
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,10 @@ const multer = require('multer');
 const router = require('express').Router();
 const ProblemReport = require('../models/report'); // Adjust the path accordingly
 
+const User1 = require('../models/user.models')
+const {authSchema} = require('../helpers/auth_schema')
+const CreateHttpError = require('http-errors')
+
 // Set up multer storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -39,3 +43,68 @@ router.post('/user', upload.single('image'), async (req, res) => {
 // ... (rest of your code)
 
 module.exports = router;
+    try{
+        const result = await authSchema.validateAsync(req.body)
+        const user = await User.findOne({email:result.email})
+        if(!user) throw CreateHttpError.NotFound("Invalid email")
+
+        const isMatch =  await user.isValidPassword(result.password)
+        if(!isMatch) throw CreateHttpError.Unauthorized("Username/password invalid")
+       
+        //const accessToken =  await signAccessToken(user.id)
+        console.log("working")
+
+       res.redirect('./home')
+   
+    }
+
+    catch(error){
+        if(error.isJoi === true) {
+        return next(CreateHttpError.BadRequest("Invalid email/password"))
+        }
+        next(error)
+    }
+// })
+
+
+
+router.get('/register',async(req,res,next)=>{
+    res.render("register")
+})
+
+router.post('/register',async(req,res,next)=>{
+
+    try{
+   const {email,password} = req.body   
+
+   const result = await authSchema.validateAsync(req.body) 
+     
+    console.log(result)
+
+    //if(!email||!password) throw CreateError.BadRequest()
+
+   const doesExist = await User1.findOne({'email':result.email})
+    if(doesExist) throw CreateHttpError.Conflict(`${result.email} already exist`)
+
+    const user = new User1(result)
+   const savedUser = await user.save()
+   //const accessToken = await signAccessToken(savedUser.id)
+   res.redirect('/home')
+     }
+    catch(error){
+        
+       next(error)
+   }
+})
+
+router.get('/user',async(req,res,next)=>{
+    res.render('report')
+})
+
+router.get('/logout',async(req,res,next)=>{
+    req.logout()
+    res.redirect('/')
+})
+
+
+module.exports = router
